0331eec84007fc783aaaeb2fdab8d93cf7ef1a98
k.koide <k.koide@aist.go.jp>
2020-03-09 16:49:38 +0900

    fix a loading bug

--- src/hdl_graph_slam/keyframe.cpp
+++ src/hdl_graph_slam/keyframe.cpp
@@ -8,27 +8,13 @@
 
 namespace hdl_graph_slam {
 
-KeyFrame::KeyFrame(const ros::Time& stamp, const Eigen::Isometry3d& odom, double accum_distance, const pcl::PointCloud<PointT>::ConstPtr& cloud)
-  : stamp(stamp),
-    odom(odom),
-    accum_distance(accum_distance),
-    cloud(cloud),
-    node(nullptr)
-{}
+KeyFrame::KeyFrame(const ros::Time& stamp, const Eigen::Isometry3d& odom, double accum_distance, const pcl::PointCloud<PointT>::ConstPtr& cloud) : stamp(stamp), odom(odom), accum_distance(accum_distance), cloud(cloud), node(nullptr) {}
 
-KeyFrame::KeyFrame(const std::string& directory, g2o::HyperGraph* graph)
-    : stamp(),
-      odom(Eigen::Isometry3d::Identity()),
-      accum_distance(-1),
-      cloud(nullptr),
-      node(nullptr)
-{
-    load(directory, graph);
+KeyFrame::KeyFrame(const std::string& directory, g2o::HyperGraph* graph) : stamp(), odom(Eigen::Isometry3d::Identity()), accum_distance(-1), cloud(nullptr), node(nullptr) {
+  load(directory, graph);
 }
 
-KeyFrame::~KeyFrame() {
-
-}
+KeyFrame::~KeyFrame() {}
 
 void KeyFrame::save(const std::string& directory) {
   if(!boost::filesystem::is_directory(directory)) {
@@ -51,15 +37,15 @@ void KeyFrame::save(const std::string& directory) {
   }
 
   if(utm_coord) {
-      ofs << "utm_coord " << utm_coord->transpose() << "\n";
+    ofs << "utm_coord " << utm_coord->transpose() << "\n";
   }
 
   if(acceleration) {
-      ofs << "acceleration " << acceleration->transpose() << "\n";
+    ofs << "acceleration " << acceleration->transpose() << "\n";
   }
 
   if(orientation) {
-      ofs << "orientation " << orientation->w() << " " << orientation->x() << " " << orientation->y() << " " << orientation->z() << "\n";
+    ofs << "orientation " << orientation->w() << " " << orientation->x() << " " << orientation->y() << " " << orientation->z() << "\n";
   }
 
   if(node) {
@@ -70,108 +56,104 @@ void KeyFrame::save(const std::string& directory) {
 }
 
 bool KeyFrame::load(const std::string& directory, g2o::HyperGraph* graph) {
-    std::ifstream ifs(directory + "/data");
-    if(!ifs) {
-        return false;
-    }
+  std::ifstream ifs(directory + "/data");
+  if(!ifs) {
+    return false;
+  }
 
-    long node_id = -1;
-    boost::optional<Eigen::Isometry3d> estimate;
+  long node_id = -1;
+  boost::optional<Eigen::Isometry3d> estimate;
 
-    while(!ifs.eof()) {
-        std::string token;
-        ifs >> token;
+  while(!ifs.eof()) {
+    std::string token;
+    ifs >> token;
 
-        if(token == "stamp") {
-            ifs >> stamp.sec >> stamp.nsec;
-        } else if(token == "estimate") {
-            Eigen::Matrix4d mat;
-            for(int i=0; i<4; i++) {
-                for(int j=0; j<4; j++) {
-                    ifs >> mat(i, j);
-                }
-            }
-            estimate = Eigen::Isometry3d::Identity();
-            estimate->linear() = mat.block<3, 3>(0, 0);
-            estimate->translation() = mat.block<3, 1>(0, 3);
-        } else if(token == "odom") {
-            Eigen::Matrix4d odom_mat = Eigen::Matrix4d::Identity();
-            for(int i=0; i<4; i++) {
-                for(int j=0; j<4; j++) {
-                    ifs >> odom_mat(i, j);
-                }
-            }
-
-            odom.setIdentity();
-            odom.linear() = odom_mat.block<3, 3>(0, 0);
-            odom.translation() = odom_mat.block<3, 1>(0, 3);
-        } else if(token == "accum_distance") {
-            ifs >> accum_distance;
-        } else if(token == "floor_coeffs") {
-            Eigen::Vector4d coeffs;
-            ifs >> coeffs[0] >> coeffs[1] >> coeffs[2] >> coeffs[3];
-            floor_coeffs = coeffs;
-        } else if (token == "utm_coord") {
-            Eigen::Vector3d coord;
-            ifs >> coord[0] >> coord[1] >> coord[2];
-            utm_coord = coord;
-        } else if(token == "acceleration") {
-            Eigen::Vector3d acc;
-            ifs >> acc[0] >> acc[1] >> acc[2];
-            acceleration = acc;
-        } else if(token == "orientation") {
-            Eigen::Quaterniond quat;
-            ifs >> quat.w() >> quat.x() >> quat.y() >> quat.z();
-            orientation = quat;
-        } else if(token == "id") {
-            ifs >> node_id;
+    if(token == "stamp") {
+      ifs >> stamp.sec >> stamp.nsec;
+    } else if(token == "estimate") {
+      Eigen::Matrix4d mat;
+      for(int i = 0; i < 4; i++) {
+        for(int j = 0; j < 4; j++) {
+          ifs >> mat(i, j);
         }
+      }
+      estimate = Eigen::Isometry3d::Identity();
+      estimate->linear() = mat.block<3, 3>(0, 0);
+      estimate->translation() = mat.block<3, 1>(0, 3);
+    } else if(token == "odom") {
+      Eigen::Matrix4d odom_mat = Eigen::Matrix4d::Identity();
+      for(int i = 0; i < 4; i++) {
+        for(int j = 0; j < 4; j++) {
+          ifs >> odom_mat(i, j);
+        }
+      }
+
+      odom.setIdentity();
+      odom.linear() = odom_mat.block<3, 3>(0, 0);
+      odom.translation() = odom_mat.block<3, 1>(0, 3);
+    } else if(token == "accum_distance") {
+      ifs >> accum_distance;
+    } else if(token == "floor_coeffs") {
+      Eigen::Vector4d coeffs;
+      ifs >> coeffs[0] >> coeffs[1] >> coeffs[2] >> coeffs[3];
+      floor_coeffs = coeffs;
+    } else if(token == "utm_coord") {
+      Eigen::Vector3d coord;
+      ifs >> coord[0] >> coord[1] >> coord[2];
+      utm_coord = coord;
+    } else if(token == "acceleration") {
+      Eigen::Vector3d acc;
+      ifs >> acc[0] >> acc[1] >> acc[2];
+      acceleration = acc;
+    } else if(token == "orientation") {
+      Eigen::Quaterniond quat;
+      ifs >> quat.w() >> quat.x() >> quat.y() >> quat.z();
+      orientation = quat;
+    } else if(token == "id") {
+      ifs >> node_id;
     }
+  }
 
-    if(node_id < 0) {
-        ROS_ERROR_STREAM("invalid node id!!");
-        ROS_ERROR_STREAM(directory);
-        return false;
-    }
+  if(node_id < 0) {
+    ROS_ERROR_STREAM("invalid node id!!");
+    ROS_ERROR_STREAM(directory);
+    return false;
+  }
 
-    node = dynamic_cast<g2o::VertexSE3*>(graph->vertices()[node_id]);
-    if(node == nullptr) {
-        ROS_ERROR_STREAM("failed to downcast!!");
-        return false;
-    }
+  if(graph->vertices().find(node_id) == graph->vertices().end()) {
+    ROS_ERROR_STREAM("vertex ID=" << node_id << " does not exist!!");
+    return false;
+  }
 
-    if(estimate) {
-        node->setEstimate(*estimate);
-    }
+  node = dynamic_cast<g2o::VertexSE3*>(graph->vertices()[node_id]);
+  if(node == nullptr) {
+    ROS_ERROR_STREAM("failed to downcast!!");
+    return false;
+  }
 
-    pcl::PointCloud<PointT>::Ptr cloud_(new pcl::PointCloud<PointT>());
-    pcl::io::loadPCDFile(directory + "/cloud.pcd", *cloud_);
-    cloud = cloud_;
+  if(estimate) {
+    node->setEstimate(*estimate);
+  }
 
-    return true;
+  pcl::PointCloud<PointT>::Ptr cloud_(new pcl::PointCloud<PointT>());
+  pcl::io::loadPCDFile(directory + "/cloud.pcd", *cloud_);
+  cloud = cloud_;
+
+  return true;
 }
 
 long KeyFrame::id() const {
-    return node->id();
+  return node->id();
 }
 
 Eigen::Isometry3d KeyFrame::estimate() const {
   return node->estimate();
 }
 
-KeyFrameSnapshot::KeyFrameSnapshot(const Eigen::Isometry3d& pose, const pcl::PointCloud<PointT>::ConstPtr& cloud)
-  : pose(pose),
-    cloud(cloud)
-{}
+KeyFrameSnapshot::KeyFrameSnapshot(const Eigen::Isometry3d& pose, const pcl::PointCloud<PointT>::ConstPtr& cloud) : pose(pose), cloud(cloud) {}
 
-KeyFrameSnapshot::KeyFrameSnapshot(const KeyFrame::Ptr& key)
-  : pose(key->node->estimate()),
-    cloud(key->cloud)
-{}
+KeyFrameSnapshot::KeyFrameSnapshot(const KeyFrame::Ptr& key) : pose(key->node->estimate()), cloud(key->cloud) {}
 
+KeyFrameSnapshot::~KeyFrameSnapshot() {}
 
-KeyFrameSnapshot::~KeyFrameSnapshot() {
-
-}
-
-}
+}  // namespace hdl_graph_slam
