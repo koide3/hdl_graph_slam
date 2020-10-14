f3a56188589806c70bee5f0e9d71efcf679763f0
Micho Radovnikovich <mradov@gmail.com>
2020-10-14 01:36:35 -0400

    Use rospy and setup.py to manage shebangs for Python 2 and Python 3
    Following guidance found here: http://wiki.ros.org/UsingPython3/SourceCodeChanges#Changing_shebangs

---
+++ setup.py
@@ -0,0 +1,12 @@
+## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+
+from setuptools import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+# fetch values from package.xml
+setup_args = generate_distutils_setup(
+    packages=['hdl_graph_slam'],
+    package_dir={'': 'src'})
+
+setup(**setup_args)
+
